4816bf97c37a6110caf80a6e92636ccc1221c5d4
dionysus0214 <hiba0214@naver.com>
2021-07-17 14:44:31 +0900

    검색결과 구현(JS)

--- 1_vanilla/js/models/SearchModel.js
+++ 1_vanilla/js/models/SearchModel.js
@@ -2,12 +2,12 @@ const data = [
   {
     id: 1,
     name: '[키친르쎌] 홈메이드 칠리소스 포크립 650g',
-    image: 'https://cdn.bmf.kr/_data/product/H1821/5a4ed4e8a6751cb1cc089535c000f331.jpg'
+    image: 'https://www.chungjungone.com/knowhow/images/blog/recipe/r220_20161122/1.jpg'
   },
   {
     id: 2,
     name: '[키친르쎌] 이탈리아 파티 세트 3~4인분',
-    image: 'https://cdn.bmf.kr/_data/product/H503E/300d931e3b8252ed628b6a3c2f56936b.jpg'
+    image: 'https://lh3.googleusercontent.com/proxy/Yu7wjvcewtzrPeLdWCSismrk6yas5m4qh8U6GQc-iC34YR_kDUVtDEkNtRo9aGRWTgdp46qnRw8An-B7OVQQ_aP_HCoTenom2RUmBiNouME8U0U-nSU'
   }
 ]
 
