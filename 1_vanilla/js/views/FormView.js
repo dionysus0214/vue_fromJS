991afa9ae581602d68718463bd529641a709e8d2
dionysus0214 <hiba0214@naver.com>
2021-12-12 14:27:30 +0900

    검색어 입력 시 x버튼 보임, 엔터 클릭 시 검색 결과 전송(JS)

--- 1_vanilla/js/views/FormView.js
+++ 1_vanilla/js/views/FormView.js
@@ -4,7 +4,8 @@ const tag = '[FormView]'
 
 const FormView = Object.create(View)
 
-FormView.setup = function(el) {
+FormView.setup = function (el) {
+  // HTML element를 주입받아서 내부적으로 속성으로 가짐
   this.init(el)
   this.inputEl = el.querySelector('[type=text]')
   this.resetEl = el.querySelector('[type=reset]')
@@ -24,11 +25,11 @@ FormView.bindEvents = function() {
 }
 
 FormView.onKeyup = function(e) {
-  const enter = 13
-  this.showResetBtn(this.inputEl.value.length)
+  const enter = 13 // enterkey는 키값이 13
+  this.showResetBtn(this.inputEl.value.length) // 입력값이 있을 때만 버튼 보임
   if(!this.inputEl.value.length) this.emit('@reset')
   if(e.keyCode !== enter) return 
-  this.emit('@submit', {input: this.inputEl.value})
+  this.emit('@submit', {input: this.inputEl.value}) // MainController에 입력값 전송
 }
 
 FormView.onClickReset = function() {
